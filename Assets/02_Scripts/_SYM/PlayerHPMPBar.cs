d95a42c748594f7f3a185238d27b73b3dec0b473
YunMiiiii <139723213+YunMiiiii@users.noreply.github.com>
2023-11-02 17:31:47 +0900

    add : 체력바 스크립트
    skillCoolTime.cs 추가

---
+++ Assets/02_Scripts/_SYM/PlayerHPMPBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPMPBar : MonoBehaviour
+{
+    public Slider hp_Slider;
+    public Slider mp_Slider;
+
+    public Text hp_text;
+    public Text mp_text;
+    public Player PlayerState;
+
+    //int hp_value;
+    //int mp_value;
+
+
+    void Start()
+    {
+        hp_Slider = GameObject.Find("hp_Slider").GetComponent<Slider>();
+        mp_Slider = GameObject.Find("mp_Slider").GetComponent<Slider>();
+        hp_Slider.minValue = 0;
+        mp_Slider.minValue = 0;
+
+    }
+
+
+
+    private void Update()
+    {
+        hp_Slider.maxValue = PlayerState.Max_Hp;
+        mp_Slider.maxValue = PlayerState.Max_Mp;
+        hp_Slider.value = PlayerState.Cur_Hp;
+        mp_Slider.value = PlayerState.Cur_Mp;
+    }
+
+}
