e06fc3e22d1fa88664474718c18daf2cd8ee9686
arena5045 <dnwjdtj10@naver.com>
2023-11-02 14:48:22 +0900

    플레이어 간단하게 구현 ++ 플레이어 스크립트 추가

---
+++ Assets/02_Scripts/_SJW/Player.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum PlayerState
+    { 
+    Idle,
+    Run,
+    Dash,
+    Casting,
+    Attack,
+    Skill,
+    Death
+    }
+    public PlayerState state;
+    private Animator animator;
+
+    [SerializeField]
+    private int max_hp;
+    [SerializeField]
+    private int max_mp;
+
+    [SerializeField]
+    private int cur_hp;
+    [SerializeField]
+    private int cur_mp;
+
+    [SerializeField]
+    private int str;//공격력
+    [SerializeField]
+    private int igt;//마법공격력
+    [SerializeField]
+    private int dex;//민첩
+    [SerializeField]
+    private int def;//방어력
+
+    [SerializeField]
+    float attack_speed;//공격속도
+    [SerializeField]
+    float move_speed;//이동속도
+
+
+    public int Max_Hp {  get { return max_hp; } set { max_hp = value; } }
+    public int Max_Mp { get { return max_mp; } set { max_mp = value; } }
+    public int Cur_Hp { get { return cur_hp; } set { cur_hp = value; } }
+    public int Cur_Mp { get { return cur_mp; } set { cur_mp = value; } }
+
+    public int Str { get { return str; } set { str = value; } }
+    public int Igt { get { return igt; } set { igt = value; } }
+    public int Dex { get { return dex; } set { dex = value; } }
+    public int Def { get { return def; } set { def = value; } }
+
+    public float Attack_speed { get { return attack_speed; } set { attack_speed = value; } }
+    public float Move_Speed { get { return move_speed; } set { move_speed = value; } }
+
+
+
+    public void SetState() 
+    {
+        Max_Hp = 1000;
+        Max_Mp = 1000;
+        cur_hp = Max_Hp;
+        cur_mp = Max_Mp;
+
+        Str = 10;
+        Igt = 10;
+        Dex = 10;
+        Def = 10;
+
+        Attack_speed = 10f;
+        Move_Speed = 3.5f;
+    }
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        SetState();
+
+
+    }
+
+    private void Start()
+    {
+        state = PlayerState.Idle;
+    }
+
+
+
+    public void PlayerRun()
+    {
+        if(state != PlayerState.Run) 
+        {
+            state = PlayerState.Run;
+            animator.SetTrigger("DoRun");
+        }
+    }
+
+    public void PlayerIdle()
+    {
+        if (state != PlayerState.Idle)
+        {
+            state = PlayerState.Idle;
+            animator.SetTrigger("DoIdle");
+        }
+    }
+}
--- Assets/02_Scripts/_SJW/PlayerMovement.cs
+++ Assets/02_Scripts/_SJW/PlayerMovement.cs
@@ -9,42 +9,50 @@ public class PlayerMovement : MonoBehaviour
     private Camera cam;
     private NavMeshAgent agent;
 
+
+    [SerializeField]
     private bool isMove;
-    private Vector3 destination;
 
     [SerializeField]
     private Transform playerCharacter;//플레이어 캐릭터
 
-
+    Player player;
     private void Awake()
     {
         cam = Camera.main;
+       
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
+
+        player = GetComponent<Player>();
     }
 
     void Update()
     {
+
+        LookMoveDirection();
+
+
         if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
             if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                if(hit.transform.gameObject.layer == 7 )
+                if (hit.transform.gameObject.layer == 7 )
                 {
                     SetDest(hit.point);
+                    agent.velocity = agent.desiredVelocity.normalized * agent.speed;
+                    player.PlayerRun(); // 이동 시킴
                 }
-
             }
         }
 
-        LookMoveDirection();
+     
     }
 
     private void SetDest(Vector3 dest)
     {
         agent.SetDestination(dest);
-        destination = dest;
         isMove = true;
     }
 
@@ -52,16 +60,31 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isMove)
         {
-            //if (Vector3.Distance(destination, transform.position) <= 0.1f)
-            if (agent.velocity.magnitude == 0.0f)  // 변경
+            print("작동");
+            if (agent.remainingDistance == 0.0f && agent.velocity.sqrMagnitude < 0.1f * 0.1f && player.state == Player.PlayerState.Run)  // 변경
             {
+                print("남은거리" + agent.remainingDistance + "속도" + agent.velocity.sqrMagnitude);
+                print("무브끔작동");
+                player.PlayerIdle();
                 isMove = false;
+
+               
                 return;
             }
 
             // var dir = destination - transform.position;
-            var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;  // 변경
-            playerCharacter.transform.forward = dir;
+            var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position; 
+            //playerCharacter.transform.forward = dir;
+            LerfRot(dir);
         }
+    }
+
+
+    public void LerfRot(Vector3 dir)
+    {
+        var targetRotation = Quaternion.LookRotation(dir); // 목표 회전값 계산
+
+        // 부드럽게 회전하기 위해 Quaternion.Slerp 사용
+        playerCharacter.transform.rotation = Quaternion.Slerp(playerCharacter.transform.rotation, targetRotation, 10 * Time.deltaTime);
     }
 }
