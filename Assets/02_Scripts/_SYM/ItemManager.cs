b46e721427a19273ef8195b4e2cf47765b3cc64f
YunMiiiii <139723213+YunMiiiii@users.noreply.github.com>
2023-11-09 16:04:33 +0900

    인벤토리 UI 추가 및 수정

--- Assets/02_Scripts/_SYM/ItemManager.cs
+++ Assets/02_Scripts/_SYM/ItemManager.cs
@@ -1,45 +1,49 @@
 using DarkLandsUI.Scripts.Equipment;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
-public class InventoryManager : MonoBehaviour
+public class ItemManager: MonoBehaviour
 {
     public Transform slotPanel;
     public GameObject slotPrefab;
     public Text itemDescriptionText; // 아이템 설명 텍스트
     public Text weaponExplanationText;
 
-    PlayerItemManager ItemManager;
-
-    void Start()
+    // 아이템을 인벤토리에 추가하는 메서드
+    public List<ItemData> AddItem(List<ItemData> items, ItemData newItem)
     {
-        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-        if (playerObject != null)
+        // 인벤토리에서 같은 아이템을 찾습니다.
+        var existingItem = items.Find(item => item.ItemName == newItem.ItemName);
+        if (existingItem != null)
         {
-            ItemManager = playerObject.GetComponent<PlayerItemManager>();
-        }
-        if (ItemManager != null)
-        {
-            InitializeInventory();
+            // 같은 아이템이 있으면, 수량을 업데이트합니다.
+            existingItem.quantity += newItem.quantity;
         }
         else
         {
-            Debug.LogError("Player 오브젝트에 PlayerScript가 없습니다.");
+            // 새 아이템이면 리스트에 추가합니다.
+            items.Add(newItem);
         }
+
+        // UI 업데이트 로직 호출
+        InitializeInventory(items);
+
+        return items;
     }
 
-    void InitializeInventory()
+    void InitializeInventory(List<ItemData> items)
     {
-        foreach (ItemData item in ItemManager.items)
+        foreach (ItemData item in items)
         {
             GameObject instance = Instantiate(slotPrefab, slotPanel);
-            //슬롯 프리팹에 아이템 정보 설정
+            // 슬롯 프리팹에 아이템 정보 설정
             instance.transform.Find("ItemImage").GetComponent<Image>().sprite = item.Item_Icon;
-            //instance.transform.Find("ItemQuantity").GetComponent<Text>().text = item.quantity.ToString();
+            //instance.transform.Find("ItemName").GetComponent<Text>().text = item.ItemName;
             instance.transform.Find("explanation").GetComponent<Text>().text = item.explanation;
 
             if (item is E_Item)
@@ -47,16 +51,9 @@ public class InventoryManager : MonoBehaviour
                 GameObject instance1 = Instantiate(slotPrefab, slotPanel);
                 //장비템일경우 스텟 상승치를 text에 띄워둠
                 instance.transform.Find("ItemImage").GetComponent<Image>().sprite = item.Item_Icon;
-                //instance.transform.Find("ItemQuantity").GetComponent<Text>().text = item.quantity.ToString();
+                //instance.transform.Find("ItemName").GetComponent<Text>().text = item.ItemName;
                 instance1.transform.Find("WeaponExplanation").GetComponent<Text>().text = item.explanation;
-
-
             }
-
-
         }
-
     }
 }
-
-
--- Assets/02_Scripts/_SYM/PlayerLevelText.cs
+++ Assets/02_Scripts/_SYM/PlayerLevelText.cs
@@ -8,11 +8,26 @@ public class PlayerLevelText : MonoBehaviour
 {
     public TMP_Text PlayerLevText;
 
-    public Player player;
+    Player player;
+    private Player playerScript;
 
     void Start()
     {
-        PlayerLevText = GameObject.Find("PlayerLevel").GetComponent<TMP_Text>();
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            // 해당 오브젝트에 붙어 있는 스크립트를 가져옵니다.
+            player = playerObject.GetComponent<Player>();
+        }
+        // 사용 예시: 스크립트가 성공적으로 찾아졌는지 확인하고, 이를 사용합니다.
+        if (player != null)
+        {
+            PlayerLevText = GameObject.Find("PlayerLevel").GetComponent<TMP_Text>();
+        }
+        else
+        {
+            Debug.LogError("Player 오브젝트에 PlayerScript가 없습니다.");
+        }
     }
 
     private void Update()
