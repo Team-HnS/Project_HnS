de82a0bb17f727f3e509d11a402e481b7592ed49
ARENA\arena5045 <dnwjdtj10@naver.com>
2023-11-12 03:20:41 +0900

    데미지 ease에 두트윈 추가

--- Assets/02_Scripts/_SJW/DamageFont.cs
+++ Assets/02_Scripts/_SJW/DamageFont.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class DamageFont : MonoBehaviour
 {
     float Damage;
     public float speed = 1.0f;
-    private float timer = 1.0f;
-
+    private float timer = 3.0f;
     public TMP_Text Dmgtxt;
-
+    Sequence damageseqence;
     private void Awake()
     {
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        damageseqence = DOTween.Sequence()
+            .Append(transform.DOMoveY(transform.position.y + Random.Range(0.5f, 2f), 1.5f).SetEase(Ease.OutBack))
+            .Join(transform.DOMoveX(transform.position.x + Random.Range(-3f, 3f), 1.5f))
+            .Join(transform.DOScale(0.7f, 0.25f))
+            .Join(Dmgtxt.DOFade(0,3f));
     }
     public void Damagesetting(float Damage)
     {
@@ -44,10 +49,13 @@ public class DamageFont : MonoBehaviour
         Dmgtxt.text = "+" + ((int)result).ToString() + " E";
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+      
+
+        //transform.Translate(Vector3.up * speed * Time.deltaTime);
             Destroy(gameObject , timer);
     }
 }
