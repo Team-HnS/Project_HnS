ccced721e723695f3d0c6be1f26244306d0f42eb
YunMiiiii <139723213+YunMiiiii@users.noreply.github.com>
2023-11-15 17:51:24 +0900

    히히

--- Assets/02_Scripts/_SYM/Item/ItemManager.cs
+++ Assets/02_Scripts/_SYM/Item/ItemManager.cs
@@ -16,7 +16,8 @@ public class ItemManager : MonoBehaviour
     public List<ItemData> items;
     public List<Slot> Slots;
     public int countItem;
-    public int playerCoins;
+    public int PlayerCoin = 0;
+
 
     Slot slot;
 
@@ -29,7 +30,7 @@ public class ItemManager : MonoBehaviour
     public Transform slotPanel;
     public GameObject slotPrefab;
     //public Text weaponExplanationText;
-    
+
 
     private void Awake()
     {
@@ -43,52 +44,52 @@ public class ItemManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-
-    //public void AddCoin(int amount, Coin coin)
+    //public void AddCoin(Coin coin)
     //{
-    //    if(GameObject.FindObjectOfType<Coin>() != null)
-    //    {
 
-    //    }
-    //    playerCoins.coin += amount; // 코인 수량 증가
+    //    PlayerCoin += coin.coin;
+    //    Debug.Log(PlayerCoin.ToString());
 
-    //    UpdateCoinUI(playerCoins.coin); // 코인 UI 업데이트
+    //    UpdateCoinUI(PlayerCoin);
     //}
 
-    //private void UpdateCoinUI(int coinCount)
-    //{
-    //    if (coinCountText != null)
-    //    {
-    //        coinCountText.text = "Coins: " + coinCount.ToString();
-    //    }
-    //}
+    private void UpdateCoinUI(int coinCount)
+    {
+        coinCountText.text = "Coins: " + coinCount.ToString();
+    }
 
     public void AddItem(ItemData newItem, int quantity)
     {
-        //if (newItem is Coin)
+        //if (newItem is Coin) // 코인 아이템인 경우
         //{
-        //    AddCoin(quantity); // 코인 아이템인 경우 AddCoin 메서드 호출
-        //    return;
+        //    Debug.Log(newItem is Coin);
+        //    AddCoin(newItem as Coin);
         //}
-
-        if (Item_data.ContainsKey(newItem))
-        {
-            // 아이템이 이미 존재하면, 수량을 증가시킵니다.
-            Item_data[newItem] += quantity;
-            Debug.Log(newItem.name);
-        }
         else
         {
-            Debug.Log(newItem.name);
-            // 새로운 아이템을 추가하고, 해당 수량을 설정합니다.
-            Item_data[newItem] = quantity;
-            items.Add(newItem); // 아이템 리스트에도 추가합니다.
+
+            if (Item_data.ContainsKey(newItem))
+            {
+                // 아이템이 이미 존재하면, 수량을 증가시킵니다.
+                Item_data[newItem] += quantity;
+                Debug.Log(newItem.name);
+                if (items is null)
+                {
+                    Item_data[newItem] = quantity;
+                    items.Add(newItem);
+                }
+            }
+            else
+            {
+                Debug.Log(newItem.name);
+                // 새로운 아이템을 추가하고, 해당 수량을 설정합니다.
+                Item_data[newItem] = quantity;
+                items.Add(newItem); // 아이템 리스트에도 추가합니다.
+            }
+
+            countItem = CalculateTotalItemCount(); // 전체 아이템 수 업데이트
         }
-
-        countItem = CalculateTotalItemCount(); // 전체 아이템 수 업데이트
     }
-
     private int CalculateTotalItemCount()
     {
         int total = 0;
