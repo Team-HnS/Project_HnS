773ba024cfd42de62f1d9ae78a918fdb5ce52b48
arena5045 <dnwjdtj10@naver.com>
2023-11-16 10:39:41 +0900

    인풋 에러 수정

--- Assets/02_Scripts/_SJW/스킬효과/SkillCollider.cs
+++ Assets/02_Scripts/_SJW/스킬효과/SkillCollider.cs
@@ -6,6 +6,7 @@ public class SkillCollider : MonoBehaviour
 {
     public Collider range;
     public int damage;
+    public GameObject HitEffect;
 
     private void Awake()
     {
@@ -24,6 +25,14 @@ public class SkillCollider : MonoBehaviour
         {
             other.gameObject.GetComponent<Monster>().Damaged(damage);
             Debug.Log(other.name + "스킬히트! " + damage + "의 데미지!");
+
+            if (HitEffect != null)
+            {
+                Vector3 contactPoint = other.ClosestPointOnBounds(transform.position);
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, transform.up);
+
+                Instantiate(HitEffect, contactPoint, rotation);
+            }
         }
     }
 
