88d298510b199868172006ef1db2188054ac4b0a
arena5045 <dnwjdtj10@naver.com>
2023-11-17 16:44:18 +0900

    ui 씬로드 구현중..

--- Assets/02_Scripts/_SJW/SkillSet_Library.cs
+++ Assets/02_Scripts/_SJW/SkillSet_Library.cs
@@ -13,7 +13,7 @@ using static UnityEngine.GraphicsBuffer;
 public class SkillSet_Library : MonoBehaviour
 {
    
-    public static SkillSet_Library instance;
+    public static SkillSet_Library instance = null;
 
     public GameObject player;
     public Player player_s;
@@ -23,10 +23,28 @@ public class SkillSet_Library : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
-        player = FindObjectOfType<Player>().gameObject;
-        player_s = player.GetComponent<Player>();
-        player_m = player.GetComponent<PlayerMovement>();
+            if (null == instance)
+            {
+                //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
+                instance = this; 
+                player = FindObjectOfType<Player>().gameObject;
+                player_s = player.GetComponent<Player>();
+                player_m = player.GetComponent<PlayerMovement>();
+
+            //씬 전환이 되더라도 파괴되지 않게 한다.
+            //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
+            //나는 헷갈림 방지를 위해 this를 붙여주기도 한다.
+            DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+            Debug.Log("중복파괴됨");
+                //만약 씬 이동이 되었는데 그 씬에도 Hierarchy에 GameMgr이 존재할 수도 있다.
+                //그럴 경우엔 이전 씬에서 사용하던 인스턴스를 계속 사용해주는 경우가 많은 것 같다.
+                //그래서 이미 전역변수인 instance에 인스턴스가 존재한다면 자신(새로운 씬의 GameMgr)을 삭제해준다.
+                Destroy(this.gameObject);
+            }
+
     }
 
     bool SkillCostCheck(int cost) //마나코스트
